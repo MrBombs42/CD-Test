5ac8006bfb731831400f6b165a79bb8ec84a4559
MrBombs <stemzan@gmail.com>
2020-10-25 19:02:11 -0300

    Update position

--- Assets/Scripts/Selectable/SelectableManager.cs
+++ Assets/Scripts/Selectable/SelectableManager.cs
@@ -10,6 +10,7 @@ namespace CD_Test.Assets.Scripts.Selectable
         [SerializeField] private Camera _camera;
         [SerializeField] private SelectableView _view;
         [SerializeField] private float _scaleSpeed = 10;
+          [SerializeField] private float _translationSpeed = 10;
 
         private Transform _selectedTransform;
 
@@ -26,18 +27,33 @@ namespace CD_Test.Assets.Scripts.Selectable
 
             if(_selectedTransform != null){
                 UpdateScale();
+                UpdateRotation();
+                UpdatePosition();
             }
         }
 
         private void UpdateScale(){      
 
             var scrollDelta = Input.mouseScrollDelta;            
-            if(scrollDelta.sqrMagnitude > 0){
-                var speed = _scaleSpeed * scrollDelta.y * Time.deltaTime;
-                var scale = new Vector3(speed, speed, speed);
-                _selectedTransform.localScale += scale;            
-            }      
-            
+            var speed = _scaleSpeed * scrollDelta.y * Time.deltaTime;
+            var scale = new Vector3(speed, speed, speed);
+            _selectedTransform.localScale += scale;              
+        }
+
+        private void UpdateRotation(){
+            if(!Input.GetMouseButton(1)){
+                return;
+            }
+
+
+        }
+
+        private void UpdatePosition(){
+            var x = Input.GetAxisRaw("Horizontal");
+            var z = Input.GetAxisRaw("Vertical");
+            var y = Input.GetAxisRaw("UpDown");
+
+            _selectedTransform.position += new Vector3(x, y, z) * _translationSpeed * Time.deltaTime;
         }
 
     }
