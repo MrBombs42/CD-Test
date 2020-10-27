2a1880b4ead78f42b15316e276db4fe68c3ee6a9
MrBombs <stemzan@gmail.com>
2020-10-26 22:32:34 -0300

    WIP changing color

--- Assets/Scripts/UI/SelectableView.cs
+++ Assets/Scripts/UI/SelectableView.cs
@@ -9,11 +9,19 @@ namespace CD_Test.Assets.Scripts.UI
        
       [SerializeField] private Text _selectedObjectText;
       [SerializeField] private Button _duplicateButton;
+      [SerializeField] private Button _colorOneButton;
+      [SerializeField] private Button _colorTwoButton;
+      [SerializeField] private Button _colorThreeButton;
+
 
       public void SetSelectionName(string selectionName){
          _selectedObjectText.text = selectionName;
       }
 
       public Button DuplicateButton{get{return _duplicateButton;}}
-   }
+
+      public Button ColorOneButton{get{return _colorOneButton;}}
+      public Button ColorTwoButton { get{return _colorTwoButton;} }
+      public Button ColorThreeButton { get{return _colorThreeButton;} }
+    }
 }
